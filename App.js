950b2ec7d3f03617e583abfba0345d8a7cf356ba
rken2010 <90514981+rken2010@users.noreply.github.com>
2022-11-04 10:09:30 -0300

    feat: Se agrego TextInput y Button agregar

--- App.js
+++ App.js
@@ -2,10 +2,24 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const [textTarea, setTextTarea] = useState("");
+  const [list, setList] = useState([]);
+
   return (
     <View style={styles.container}>
-      <Text>Hola Coder!</Text>
-      <StatusBar style="auto" />
+      <Text style={{ fontSize: 30 }}>Pendientes 🛍️</Text>
+      <View>
+        <TextInput
+          placeholder="Agregar Tarea"
+          placeholderTextColor="black"
+          style={styles.input}
+          value={textTarea}
+        />
+        <Button title="Agregar" />
+      </View>
+      <View>
+      </View>
     </View>
   );
 }
@@ -17,4 +31,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input:{
+    borderBottomColor: "black",
+    borderBottomWidth: 1,
+    width: 250,
+    marginBottom:15
+  }
 });
